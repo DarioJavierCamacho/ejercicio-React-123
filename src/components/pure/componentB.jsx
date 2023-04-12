51a9983e19494a6167d682e7ff8890c598b21183
DarioJavierCamacho <Darioj1990@gmail.com>
2023-04-12 13:09:39 -0300

    entrega

--- src/components/pure/componentB.jsx
+++ src/components/pure/componentB.jsx
@@ -10,7 +10,6 @@ const ComponentB = ({ Contacto }) => {
             <h2>{ Contacto.Nombre }</h2>
             <h2>{ Contacto.Apellido }</h2>
             <h2>{ Contacto.Email }</h2>
-            <h2>{ Contacto.Conectado }</h2>
             <h2>{ Contacto.Conectado?"Contacto en Linea":"Contacto No Disponible"}</h2>
         </div>
     );
